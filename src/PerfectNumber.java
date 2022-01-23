a1eaff1f2856ce8f011a2cf1d643611f7bdc300f
Tan-int <57535667+Tan-int@users.noreply.github.com>
2022-01-23 15:56:19 -0600

    Initial commit

---
+++ src/PerfectNumber.java
@@ -0,0 +1,15 @@
+public class PerfectNumber {
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+        int sum = 0;
+
+        for(int divisor = 1; divisor < number; divisor++) {
+            if (number % divisor == 0) {
+                sum += divisor;
+            }
+        }
+        return sum == number;
+    }
+}
